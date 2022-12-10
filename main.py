b8211a268fc645769c017479fb6335b27d730ced
Zenin Easa Panthakkalakath <zeninpeasa@gmail.com>
2022-12-10 20:10:20 +0100

    Skeleton / TODO

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import sys
-sys.path.append('data/cardinal')
-sys.path.append('data/cardinal')
+sys.path.append('./data/cardinal')
+sys.path.append('./data/spatial')
+sys.path.append('./data')
+from cardinalDataGen import *
+from spatialDataGen import *
 
 import time
+
+def evaluateInModel(modelId, prompt):
+    # TODO: Implement this function
+    print('The answer')
+
+
+# TODO: Experiment runner function
+
+# TODO: Running the experiments
