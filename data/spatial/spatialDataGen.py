4429be30e9c50b44d58a5659f986f3ed73488bea
Zenin Easa Panthakkalakath <zeninpeasa@gmail.com>
2022-12-10 19:24:46 +0100

    Programmatic data generator for spatial

---
+++ data/spatial/spatialDataGen.py
@@ -0,0 +1,43 @@
+import numpy as np
+import random
+
+import sys
+sys.path.append('../')
+
+from utils import *
+
+def spatialDataGen(seed, toFile=False):
+    random.seed(seed)
+
+    if toFile:
+        f = open("spatial.txt", "w")
+    else:
+        f = StringFileInterface()
+
+    for i in range(0, 20):
+        # Size of the matrix
+        m = random.randint(2, 4)
+        n = random.randint(2, 4)
+
+        # Position of the true value
+        do = True
+        while do: # Just to ensure that xPos is not at the middle
+            xPos = random.randint(0, m - 1)
+            do = (xPos == m/2)
+        yPos = random.randint(0, n - 1)
+
+        mat = np.zeros([n, m])
+        mat[yPos, xPos] = 1
+
+        # Prompt
+        f.write('World:\n')
+        f.write(np.array2string(mat))
+        if xPos > m/2:
+            f.write('\nAnswer: right\n\n')
+        else:
+            f.write('\nAnswer: left\n\n')
+
+    if toFile:
+        f.close()
+    else:
+        return f.data
