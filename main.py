817d9a0c188af158585397df49b6ebf0718a38b3
SilvaArtUSI <silvaar@usi.ch>
2022-12-13 02:48:58 +0100

    Test code implement
    top is still missing

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import time
 import os
 
 import openai
-openai.api_key = os.environ["OPENAI_API_KEY"]
-
+from transformers import (
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+) 
 import sys
+openai.api_key = "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"
+
 sys.path.append('./data/cardinal')
 sys.path.append('./data/spatial')
 sys.path.append('./data')
@@ -14,13 +18,18 @@ from spatialDataGen import spatialDataGen
 
 numModels = 5
 numTimesRepeatExperiment = 10
+#models = (("gpt2",0),("gpt2-medium",0),("gpt2-large",0),("gpt2-xl",0),("gpt3",1))
+models = (("gpt2",0),("gpt3",1))
 
-def evaluateInModel(modelNumber, prompt):
+
+
+def evaluateInModel(modelNumber,model ,prompt,tokenizer=None):
     # TODO: include GPT-2 models
-    if (modelNumber == 0):
+
+    if (modelNumber == 1):
         # Run it on GPT-3
         prompt = prompt.strip()
-        response = openai.Completion.create(
+        response = model.create(
             engine="text-davinci-002",
             prompt=prompt,
             temperature=1,
@@ -34,32 +43,106 @@ def evaluateInModel(modelNumber, prompt):
         time.sleep(2)
         return response['choices'][0]['text']
 
-    # else:
+    elif (modelNumber == 0) :
+         # gpt2     # 12-layer, 768-hidden, 12-heads, 117M parameters.
+                    # OpenAI GPT-2 English model
+
+        
+        prompt = prompt.strip()
+        input_ids = tokenizer.encode(
+            prompt,
+            add_special_tokens=False,
+            return_tensors="pt",
+            add_space_before_punct_symbol=True
+        )
+        
+        # Check the documentation of function generate for any change in attributes. 
+        # https://huggingface.co/docs/transformers/main_classes/text_generation
+        output_ids = model.generate(
+            input_ids=input_ids,
+            do_sample=True,
+            # max_length=10,  # desired output sentence length
+            pad_token_id=model.config.eos_token_id,
+            max_new_tokens=1
+        )[0].tolist()
+
+        generated_text = tokenizer.decode(
+            output_ids,
+            clean_up_tokenization_spaces=True)
+
+
         # Run it on GPT-2, models with different sizes
-    return '???'
+        return generated_text
+    
+    return "Error"
 
 
-# TODO: Experiment runner functions
+# # TODO: Experiment runner functions
 
 def experimentWithSpatial():
     print('-' * 10)
     print(f'Experiment with spatial data:')
     statistics = np.zeros([numModels, numTimesRepeatExperiment])
+    openai.Completion
 
     for seed in range(numTimesRepeatExperiment):
         (prompt, expectedAnswer) = spatialDataGen(seed)
 
-        for i in range(numModels):
-            answer = evaluateInModel(i, prompt)
-            statistics[i, seed] = (answer == expectedAnswer)
+        #for i in range(numModels):
+            #answer = evaluateInModel(i, prompt)
+            #statistics[i, seed] = (answer == expectedAnswer)
             #print(f'Expected: {expectedAnswer}, Actual: {answer}')
 
+    return prompt,expectedAnswer
     print(np.array2string(statistics))
     print('-' * 10)
 
-# TODO: Running the experiments
+# # TODO: Running the experiments
 
-experimentWithSpatial()
 
-#(prompt, expectedAnswer) = spatialDataGen(1)
-#print(prompt)
+
+
+# #(prompt, expectedAnswer) = spatialDataGen(1)
+# #print(prompt)
+
+def run_experiment(numTimesRepeatExperiment,models,type_exp="grid"):
+    
+    tokenizer=None
+    loaded_model=None
+    statistics = np.zeros([len(models), numTimesRepeatExperiment])
+    seeds=list(range(numTimesRepeatExperiment))
+
+
+
+    for k,model_to_eval in enumerate(models):
+
+        if model_to_eval[1]==0 : #GPT2 model
+            tokenizer = GPT2Tokenizer.from_pretrained(model_to_eval[0])
+            model = GPT2LMHeadModel.from_pretrained(model_to_eval[0])
+
+            
+        else: #model[1]==1 #GPT3 model
+            model = openai.Completion
+            pass
+        
+        
+        for experiment in range(numTimesRepeatExperiment):
+
+            if type_exp=="grid":
+                (prompt, expectedAnswer) = spatialDataGen(seeds[experiment])
+
+            elif type_exp=="color":
+                (prompt, expectedAnswer) = spatialDataGen(seeds[experiment])
+        
+            elif type_exp == "cardinal":
+                (prompt, expectedAnswer) = cardinalDataGen(seeds[experiment])
+
+            #experiment
+        print(f"model: {model_to_eval[0]},exp: {k}, type: {type_exp}  ")
+        #print(f"prompt: {prompt}")
+        answer = evaluateInModel(model_to_eval[1],model ,prompt,tokenizer)
+        print(f"{model_to_eval[0]} ans: {answer}, real ans:{expectedAnswer}")
+
+run_experiment(numTimesRepeatExperiment,models,"grid")
+
+            
