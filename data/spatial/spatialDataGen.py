55bd6257d6e02856e421649950658cfe30ea2d70
Zenin Easa Panthakkalakath <zeninpeasa@gmail.com>
2022-12-17 17:09:16 +0100

    GENERALISATION TO UNSEEN WORLDS - Spatial

--- data/spatial/spatialDataGen.py
+++ data/spatial/spatialDataGen.py
@@ -7,41 +7,22 @@ sys.path.append('../')
 
 from utils import *
 
-# TODO: GENERALISATION TO UNSEEN WORLDS - final question should have world that was not used.
+# TODO: GENERALISATION TO UNSEEN CONCEPTS -
 
-# TODO: Confirm if the rotation implementation is consistent with the paper
+
+# This is for 'GENERALISATION TO UNSEEN WORLDS'
 def spatialDataGen(seed, angle=0, filename='', numTrainingPoints=20):
     random.seed(seed)
 
     f = StringFileInterface(filename)
 
     for i in range(0, numTrainingPoints + 1):
-        # Size of the matrix
-        m = random.randint(2, 7)
-        n = random.randint(1, 7)
-
-        # Position of the true value
-        do = True
-        while do: # Just to ensure that xPos is not at the middle
-            xPos = random.randint(0, m - 1)
-            do = (xPos == m/2 - 0.5)
-        yPos = random.randint(0, n - 1)
-
-        mat = np.zeros([n, m])
-        mat[yPos, xPos] = 1
-
-        # Rotate the matrix by the angle provided
-        rotatedMat = rotate(mat, angle=angle, reshape=False) # TODO: Should it be true?
-        rotatedMat[abs(rotatedMat) < 0.01] = 0
+        rotatedMat, answer = generateUniqueWorldAndAnswer(f, angle)
 
         # Prompt
         f.write('\n\nWorld:\n')
         f.write(np.array2string(rotatedMat))
         f.write('\nAnswer:')
-        if xPos > m/2:
-            answer = 'right'
-        else:
-            answer = 'left'
 
         if i < numTrainingPoints:
             f.write(answer)
@@ -51,3 +32,35 @@ def spatialDataGen(seed, angle=0, filename='', numTrainingPoints=20):
     if not(filename):
         # TODO: Extract expected answer and prompt from f.data
         return (f.data, answer)
+
+def generateUniqueWorldAndAnswer(f, angle):
+    # Size of the matrix
+    m = random.randint(2, 7)
+    n = random.randint(1, 7)
+
+    # Position of the true value
+    do = True
+    while do: # Just to ensure that xPos is not at the middle
+        xPos = random.randint(0, m - 1)
+        do = (xPos == m/2 - 0.5)
+    yPos = random.randint(0, n - 1)
+
+    mat = np.zeros([n, m])
+    mat[yPos, xPos] = 1
+
+    # Rotate the matrix by the angle provided
+    rotatedMat = rotate(mat, angle=angle, reshape=False) # TODO: Should it be true?
+    rotatedMat[abs(rotatedMat) < 0.01] = 0
+
+    if xPos > m/2:
+        answer = 'right'
+    else:
+        answer = 'left'
+
+    # Confirm if the rotatedMat is already present in 'f';
+    # If it is, recursively call and return this function
+    ## Is np.array2string(rotatedMat) present in f.data ?
+    if (np.array2string(rotatedMat) in f.data):
+        return generateUniqueWorldAndAnswer(f, angle)
+
+    return rotatedMat, answer
